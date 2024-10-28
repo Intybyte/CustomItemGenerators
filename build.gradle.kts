82348a8926eb5082a366d948f69ac384456df0db
Intybyte <vaan.testwork@gmail.com>
2024-10-28 17:22:54 +0100

    will need to delete plugin.yml if i swich to using this

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.0-Beta1"
     id("com.gradleup.shadow") version "8.3.3"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
 
 group = "me.vaan"
@@ -18,12 +19,25 @@ repositories {
 }
 
 dependencies {
+    library(kotlin("stdlib"))
+    library("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     compileOnly("com.github.Slimefun:Slimefun4:RC-37")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.github.seggan:sf4k:0.7.1")
     implementation("org.bstats:bstats-bukkit:3.0.2")
+}
+
+bukkit {
+    name = "CustomItemGenerators"
+    main = "me.vaan.customitemgen.CustomItemGenerators"
+    apiVersion = "1.20"
+    version = project.version.toString()
+    author = "Vaan1310"
+    depend = listOf("Slimefun")
+}
+
+tasks.processResources {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 val targetJavaVersion = 21
