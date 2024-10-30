e70dbd995298dea0b9971ecfc0e234fba5b185ea
Intybyte <vaan.testwork@gmail.com>
2024-10-30 15:00:05 +0100

    Add documentation

--- src/main/kotlin/me/vaan/customitemgen/registry/MachineRegistry.kt
+++ src/main/kotlin/me/vaan/customitemgen/registry/MachineRegistry.kt
@@ -2,4 +2,7 @@ package me.vaan.customitemgen.registry
 
 import me.vaan.customitemgen.generator.ItemGenerator
 
+/**
+ * Map id to slimefun item generator
+ */
 object MachineRegistry : MutableMap<String, ItemGenerator> by HashMap()
